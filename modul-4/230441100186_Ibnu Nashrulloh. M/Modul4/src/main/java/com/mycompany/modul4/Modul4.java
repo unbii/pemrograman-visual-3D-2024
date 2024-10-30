ed09509539786be0f345bfd41fea7f70e3c66ecb
unbii <ibnu.nashrulloh.m27@gmail.com>
2024-10-30 20:06:45 +0700

    230441100186_Ibnu Nashrulloh. M_Modul-4

---
+++ modul-4/230441100186_Ibnu Nashrulloh. M/Modul4/src/main/java/com/mycompany/modul4/Modul4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modul4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Modul4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
